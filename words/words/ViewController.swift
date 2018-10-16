2ff1d710569125441ad10897518eec4ea68ed9f1
jullynobre <jullynobre1@gmail.com>
2018-10-16 11:40:09 -0300

    Initial commit

---
+++ words/words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  words
+//
+//  Created by Jully Nobre on 16/10/18.
+//  Copyright © 2018 Jully Nobre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
